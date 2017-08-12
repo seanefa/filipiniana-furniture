c246cc9f7fd38221eb454966397fea7fce98f214
eyembisi <hongkaira@gmail.com>
2017-08-12 12:02:14 +0800

    commit changes

--- admin/framework-update.php
+++ admin/framework-update.php
@@ -2,13 +2,6 @@
 session_start();
 include 'dbconnect.php';
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-  // Check connection
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-
 $id = $_SESSION['varname'];
 
 $name = $_POST['name'];
@@ -35,8 +28,7 @@ else
 if($pic==""){
 	$pic = $exist_image;
 }
-        // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 $updateSql = "UPDATE tblframeworks SET frameworkName='$name', frameworkFurnType='$type',frameDesignID='$design',materialUsedID='$material', frameworkRemarks='$remarks', frameworkPic='$pic' WHERE frameworkID=$id";
 
 if(mysqli_query($conn,$updateSql)){
