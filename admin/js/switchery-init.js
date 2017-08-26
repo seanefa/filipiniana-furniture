ce515bceeadec3db2b5c9f3ac214bb52507c8f02
unknown <efasean06@gmail.com>
2017-08-26 20:18:21 +0800

    archives]

---
+++ admin/js/switchery-init.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+  $('.js-switch').each(function() {
+      new Switchery($(this)[0], $(this).data());
+  }); 
+});
