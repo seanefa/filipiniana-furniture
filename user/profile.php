939b96881f3fa68b9e9c6d1b470f503d3b5dac67
josefpls <mjoseph.publico@gmail.com>
2017-09-12 02:06:37 +0800

    profile

---
+++ user/profile.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="image/favicon.ico" rel="icon" />
+		<title>Profile - Filipiniana Furniture Shop</title>
+		<meta name="description" content="Furniture shop">
+		<script type="text/javascript" src="js/myScript.js"></script>
+		<?php include"css.php";?>
+	</head>
+	<body style="background: #fff;">
+		<?php include "header.php";?>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="">
+				</div>
+			</div>
+		</div>
+		<?php include "scripts.php";?>
+	</body>
+</html>
