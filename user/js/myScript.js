49b358f1e125b726fbdec386157b601cac9db533
josefpls <mjoseph.publico@gmail.com>
2017-09-12 16:10:33 +0800

    profile natin ok na

--- user/js/myScript.js
+++ user/js/myScript.js
@@ -17,3 +17,15 @@ password.addEventListener("focus", function(event){
 password.addEventListener("focusout", function(event){
 	password.setAttribute("placeholder", "Password");
 });
+
+function productImgError(image) {
+	image.onerror = "";
+	image.src = "";
+	return true;
+}
+
+function promoImgError(image) {
+	image.onerror = "";
+	image.src = "";
+	return true;
+}
--- user/profile.php
+++ user/profile.php
@@ -3,6 +3,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link href="image/favicon.ico" rel="icon" />
+		<link rel="stylesheet" href="css/myStyle.css">
 		<title>Profile - Filipiniana Furniture Shop</title>
 		<meta name="description" content="Furniture shop">
 		<script type="text/javascript" src="js/myScript.js"></script>
@@ -10,12 +11,59 @@
 	</head>
 	<body style="background: #fff;">
 		<?php include "header.php";?>
-		<div class="container-fluid">
+		<br>
+		<div class="breadcrumbs">
+		</div>
+		<div class="container">
 			<div class="row">
-				<div class="">
+				<div class="col-12 col-sm-6 col-md-6 col-lg-5 col-xl-4">
+					<?php 
+					include "userconnect.php";
+					$sql="SELECT * from t";
+					?>
+					<br>
+					<div class="profilethumb">
+						<div class="row">
+							<div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+								<img src="" style="height:150px; width:150px;" alt="Product" class="img-responsive profilepic"/>
+							</div>
+							<div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7">
+								<div class="info text-center">
+									<?php
+									include "userconnect.php";
+										$sql="SELECT * from tbluser as user join tblcustomer as customer where user.userCustID = customer.CustomerID and user.userCustID = 1";
+									$result=$conn->query($sql);
+									if($result->num_rows>0){
+									while($row=$result->fetch_assoc()){
+									?>
+									<h4><?php echo "" . $row["customerLastName"] . ", " . $row["customerFirstName"] . " " . substr($row["customerMiddleName"], 0, 1);?>.</h4>
+									<h6><?php echo "" . $row["customerAddress"];?></h6>
+									<ul class="text-left">
+										<li><?php echo "" . $row["customerEmail"];?></li>
+										<li><?php echo "" . $row["customerContactNum"];?></li>
+									</ul>
+									<?php
+										}
+									}
+									$conn->close();
+									?>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+								<h4 class="text-center">Proof of Payment</h4>
+							</div>
+						</div>
+					</div>
+					<br>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-7 col-xl-8">
+					<h1 class="text-center">Order Information</h1>
 				</div>
 			</div>
-		</div>
+			</div>
 		<?php include "scripts.php";?>
 	</body>
 </html>
