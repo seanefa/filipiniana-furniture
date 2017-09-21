9ded4a8ea39ed81bde0cf6f25603a8607c9436b8
josefpls <mjoseph.publico@gmail.com>
2017-09-21 23:39:55 +0800

    profile changes

--- user/profile.php
+++ user/profile.php
@@ -26,37 +26,38 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 col-sm-6 col-md-6 col-lg-5 col-xl-4">
-				<?php 
-				include "userconnect.php";
-				$sql="SELECT customerDP from tblcustomer";
-				?>
 				<br>
 				<div class="profilethumb">
+					<?php 
+					include "userconnect.php";
+					$sql = "SELECT * from tbluser as user join tblcustomer as customer where userID = " . $_SESSION["userID"] . "";
+					$result = $conn->query($sql);
+					if($result->num_rows>0)
+					{
+						while($row=$result->fetch_assoc())
+						{
+					?>
 					<div class="row">
 						<div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
 							<img src="<?php echo "" . $row["customerDP"];?>" style="height:150px; width:150px;" alt="Product" class="img-responsive profilepic"/>
 						</div>
 						<div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7">
 							<div class="info text-center">
-								<?php
-								include "userconnect.php";
-								$sql="SELECT * from tbluser as user join tblcustomer as customer where user.userCustID = customer.customerID and user.userCustID =" . $_SESSION["userID"] . "";
-								$result=$conn->query($sql);
-								$row = $result->num_rows;
-								?>
-								<h4><?php echo "" . 	$row["customerLastName"] . ", " . $row["customerFirstName"] . " " . substr($row["customerMiddleName"], 0, 1);?>.</h4>
+								<h4><?php echo "" . $row["customerLastName"] . ", " . $row["customerFirstName"] . " " . substr($row["customerMiddleName"], 0, 1);?>.</h4>
 								<h6><?php echo "" . $row["customerAddress"];?></h6>
 								<ul class="text-left">
 									<li><?php echo "" . $row["customerEmail"];?></li>
 									<li><?php echo "" . $row["customerContactNum"];?></li>
 								</ul>
-								<?php
-								$conn->close();
-								?>
 								<a href="updateinfo.php"><button class="btn btn-primary">Update</button></a>
 							</div>
 						</div>
 					</div>
+					<?php
+						}
+					}
+					$conn->close();
+					?>
 					<br>
 					<div class="row">
 						<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
