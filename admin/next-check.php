cb9612b44ce849c60c329ad71b500c61eac9de45
traynerZero <znotsukaima@gmail.com>
2017-09-23 10:44:18 +0800

    ordering

---
+++ admin/next-check.php
@@ -0,0 +1,27 @@
+<?php 
+
+$conn = new MySQLI('localhost', 'root', '', 'filfurnituredb');
+
+$matTypeName = "";
+
+if(isset($_POST['username'])){
+	$matTypeName = strip_tags($_POST['username']);
+}
+
+
+// Company Name
+if(preg_match('/[\'\/\\\^£$%&*}()\\[\\]{@#~?><!>;:`\'\"|=_¬]/', $matTypeName)){
+	echo "Symbols not allowed";
+}
+else{
+		
+if( ctype_space($matTypeName) || substr($matTypeName, 0 , 1)==" " || substr($matTypeName, strlen($matTypeName)-1, strlen($matTypeName)) == " "){
+	echo "White Space not allowed";
+}
+else if (substr($matTypeName, 0 , 1) != " " || substr($matTypeName, strlen($matTypeName)-1 ,strlen($matTypeName) ) != "" ){
+			echo "good";
+		}
+	
+}
+
+?>
