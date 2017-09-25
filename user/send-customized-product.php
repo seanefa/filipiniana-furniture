f038655e38ca491583d8146136b7fe57d10b79fa
josefpls <mjoseph.publico@gmail.com>
2017-09-25 15:13:17 +0800

    proof n custom INC

--- user/send-customized-product.php
+++ user/send-customized-product.php
@@ -18,13 +18,17 @@ if($_FILES["blueprint"]["error"]>0){
 else{
 	move_uploaded_file($_FILES["blueprint"]["tmp_name"], date("Y-m-d") . time() . ".png");
 	$blueprint = date("Y-m-d") . time() . ".png";
-	header("Location: customize.php");
 }
 
 $sendnukes = "INSERT into tblcustomize_request(tblcustomerID, customizedPic, customizedDescription, customStatus) values('$customerID', '$blueprint', '$description', 'active')";
 
 if($conn->query($sendnukes) === true){
 	header("Location: customize.php");
+}
+else{
+	echo "query:" . $sendnukes . "<br>error:" . $conn->error;
+	echo "<br><br> SESSION value: ";
+	echo $customerID;
 }
 $conn->close();
 ?>
