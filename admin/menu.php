20a3fc652cb58173f0a6bb2e04004956f500b31f
SeanE06 <efasean06@gmail.com>
2017-10-14 03:40:02 +0800

    added session timeout

--- admin/menu.php
+++ admin/menu.php
@@ -1,7 +1,39 @@
 <?php
-
 include "titleHeader.php";
 session_start();
+
+//Expire the session if user is inactive for 30
+//minutes or more.
+$expireAfter = 30;
+ 
+//Check to see if our "last action" session
+//variable has been set.
+if(isset($_SESSION['last_action'])){
+    
+    //Figure out how many seconds have passed
+    //since the user was last active.
+    $secondsInactive = time() - $_SESSION['last_action'];
+    
+    //Convert our minutes into seconds.
+    $expireAfterSeconds = $expireAfter * 60;
+    
+    //Check to see if they have been inactive for too long.
+    if($secondsInactive >= $expireAfterSeconds){
+        //User has been inactive for too long.
+        //Kill their session.
+        session_unset();
+        session_destroy();
+        echo "<script>
+          window.location.href='/admin/login.php';
+          alert('Your session has timed out. Please login again.');
+          </script>";
+    }
+} 
+//Assign the current timestamp as the user's
+//latest activity
+$_SESSION['last_action'] = time();
+
+//Check to see if session is set to admin
 if(!isset($_SESSION["userID"]) || $_SESSION['userType']!='admin')
 {
   echo "<script>
