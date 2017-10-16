aeb41561f2b4f24d6249338ee4a4c8726dfe3654
unknown <efasean06@gmail.com>
2017-10-16 20:58:16 +0800

    on promo

--- admin/promo-display.php
+++ admin/promo-display.php
@@ -13,25 +13,24 @@ $srow = mysqli_fetch_assoc($sresult);
 
 
 echo '
-<div style="border:2px solid">
+<div style="border:1px solid">
 <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="white-box text-center"> 
-    <img src="plugins/promos/'.$row['promoImage'].'" height="300px" width="580px" alt="Unavailable" class="img-responsive"> 
+    <img src="plugins/promos/'.$row['promoImage'].'" height="250px" width="580px" alt="Unavailable" class="img-responsive"> 
     </div>
 </div>
 
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12" style="text-align: center;">
-        <h4 class="box-title">Free Product</h4>
-        
-        <p>
-    <img src="plugins/images/'.$srow['prodMainPic'].'" height="112px" width="120px" alt="Unavailable" class="img-responsive">'.$srow['productName'].'</p>
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <h3 class="box-title" style="text-align: center; margin-top:-20px;">Free Product</h3>
+    <img class="col-md-6 col-md-offset-3" src="plugins/images/'.$srow['prodMainPic'].'" height="200px" width="135px" alt="Unavailable" class="img-responsive" style="margin-top:-8px;">
     </div>
+        <h4 style="text-align: center; margin-top:12px;">'.$srow['productName'].'</h4>
 </div>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12" style="text-align: center;">
-        <h4 class="box-title">Description</h4>
-        <p>'. $row['promoDescription'].'</p>
+        <h3 class="box-title">Description</h3>
+        <h4>'. $row['promoDescription'].'</h4>
     </div>
 </div>
 
