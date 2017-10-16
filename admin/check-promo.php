634ce4a37d5b96adf660d9fc5ee62fcb356544bf
eyembisi <hongkaira@gmail.com>
2017-10-17 03:58:31 +0800

    ina

--- admin/check-promo.php
+++ admin/check-promo.php
@@ -2,13 +2,12 @@
 include "dbconnect.php";
 $date = new DateTime();
 $dateToday = date_format($date, "Y-m-d");
-
 $sql = "SELECT * FROM tblpromos;";
 $res = mysqli_query($conn,$sql);
 while($row = mysqli_fetch_assoc($res)){
 	$promo = date_create($row['promoEnd']);
 	$promo = date_format($promo, "Y-m-d");
-	if($dateToday > $promo){
+	if($dateToday >= $promo){
 		$sql = "UPDATE tblpromos SET promoStatus = 'Inactive' WHERE promoID ='" .$row['promoID']. "';";
 		mysqli_query($conn,$sql);
 		prodOnPromo($row['promoID']);
@@ -20,7 +19,7 @@ function prodOnPromo($id){
 	$sql1 = "SELECT * FROM tblprodsonpromo WHERE promoDescID = '$id'";
 	$res = mysqli_query($conn,$sql1);
 	while($row = mysqli_fetch_assoc($res)){
-		$sql = "UPDATE tblprodsonpromo SET onPromoStatus = 'Inactive' WHERE onPromoID ='" .$row['onPromoID']. "';";
+		$sql = "UPDATE tblprodsonpromo SET onPromoStatus = 'Inactive' WHERE onpromoID ='" .$row['onpromoID']. "';";
 		mysqli_query($conn,$sql);
 	}
 	return 0;
