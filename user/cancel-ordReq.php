42395ab21ecb5bbee663e79580f7c998d939749b
josefpls <mjoseph.publico@gmail.com>
2017-10-17 23:36:59 +0800

    cancel order request log

--- user/cancel-ordReq.php
+++ user/cancel-ordReq.php
@@ -3,9 +3,12 @@ session_start();
 include "userconnect.php";
 
 $id = $_GET['id'];
+$customerid = $_SESSION["userID"];
 
 $sql = "UPDATE tblorders SET orderStatus = 'Cancelled' WHERE orderID = '$id'";
+$logSQL = "INSERT into tbllogs (category, action, date, description, userID) values ('Order', 'Cancelled', '" . date("Y-m-d") . "', 'Order request #0000$id cancelled', '$customerid')";
 if(mysqli_query($conn,$sql)){
+	$conn->query($logSQL);
 	echo "<script>
 	window.location.href='orders.php';
 	alert('Successfully cancelled order request.');
